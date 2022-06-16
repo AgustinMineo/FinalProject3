8e9c8cf6a9bf57485647af600fcce5bf89e1a49d
lucascolu <lucascolu@hotmail.com>
2022-06-16 18:41:22 -0300

    Actualizacion Transferencias
    Actualizacion clase Transferencia + Pending + Rechazada + Finalizada

--- src/Transferencias/Finalizada.java
+++ src/Transferencias/Finalizada.java
@@ -1,4 +1,27 @@
-package Transferencias;
+package Transfers;
 
-public class Finalizada {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import Enums.TransferCause;
+
+public final class Finalizada extends Transferencia implements Serializable{
+    private LocalDateTime finishDate;
+
+    public Finalizada(LocalDateTime finishDate,LocalDateTime fechaDeEnvio,LocalDateTime fechaDeEstado, Nodo nodo, Enum<TransferCause>sendCause) {
+    	super(fechaDeEnvio, fechaDeEstado, nodo, sendCause);
+        this.finishDate = finishDate;
+    }
+
+    public LocalDateTime getFinishDate() {return finishDate;}
+
+    public void setFinishDate(LocalDateTime finishDate) {this.finishDate = finishDate;}
+
+    @Override
+    public String toString() {
+        return "Finalizada{" +
+                "pending=" + "aca iba pending" +
+                ", finishDate=" + finishDate +
+                '}';
+    }
 }
--- src/Transferencias/Pending.java
+++ src/Transferencias/Pending.java
@@ -1,4 +1,25 @@
-package Transferencias;
+package Transfers;
 
-public class Pending {
+import Enums.TransferCause;
+import Interface.TransferValidations;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Pending extends Transferencia implements Serializable{
+    private int validations;
+    
+
+    public Pending(LocalDateTime fechaDeEnvio, LocalDateTime fechaDeEstado, Nodo nodo,Enum<TransferCause>sendCause) {
+        super(fechaDeEnvio, fechaDeEstado, nodo,sendCause);
+        this.validations = 0;
+    }
+    
+    public Pending() {}
+
+    public int getValidations() {return validations;}
+
+    public void setValidations(int validations) {this.validations = validations;}
+    
+    public void addValidations() {this.validations++;}
 }
