671ac8938b55e87ba42a7c93e4b646e7a8b7e5fe
lucascolu <lucascolu@hotmail.com>
2022-06-20 17:28:40 -0300

    Cambios

--- src/Main.java
+++ src/Main.java
@@ -14,23 +14,40 @@ import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 
 public class Main{
+
 	public static HashMap<String,Usuario> map=new HashMap<String,Usuario>();
 	public static List<Transferencia> lista=new ArrayList<>();
 
+
 	public static void main(String[] args) {
+<<<<<<< HEAD
 
 		map=cargaHashUsuarios();
 
 		lista=cargaListaTransferencias();
 		
 		//Muestro usuarios precargados
+=======
+		/*Usuario user=new Usuario("l","123456","lucas","coluccio","38551218");
+		Usuario user2=new Usuario("a","123456","lucas","coluccio","38551219");
+		Usuario user3=new Usuario("c","123456","lucas","coluccio","38551213");
+		map.put(user.getEmail(), user);
+		map.put(user2.getEmail(), user2);
+		map.put(user3.getEmail(), user3);*/
+		map=cargaHashUsuarios();
+		lista=cargaListaTransferencias();
+>>>>>>> parent of 64e1927 (Try/catch en carga de archivos en Main)
 		for (Map.Entry<String, Usuario> set :map.entrySet()) {
 			System.out.println(set.getKey() + " = " + set.getValue());
 		}
 		BienvenidaUI nuevaInterfaz=new BienvenidaUI(map,lista);
 		nuevaInterfaz.setVisible(true);
 	}
+<<<<<<< HEAD
 	public static HashMap<String,Usuario> cargaHashUsuarios(){
+=======
+	public static HashMap<String,Usuario> cargaHashUsuarios() {
+>>>>>>> parent of 64e1927 (Try/catch en carga de archivos en Main)
 		try {
 			//JsonElement fileElement = JsonParser.parseReader(new FileReader("C:\\Users\\Agustin\\Desktop\\Cambios\\TP FINAL\\listaUsuarios.json"));
 			JsonElement fileElement = JsonParser.parseReader(new FileReader("C:\\Users\\lcoluccio\\Desktop\\GIT\\FinalProject3\\listaUsuarios.json"));
@@ -46,7 +63,11 @@ public class Main{
 			return null;
 	      }
 	}
+<<<<<<< HEAD
 	public static List<Transferencia> cargaListaTransferencias(){
+=======
+	public static List<Transferencia> cargaListaTransferencias() {
+>>>>>>> parent of 64e1927 (Try/catch en carga de archivos en Main)
 		try {
 	         //FileInputStream fileIn = new FileInputStream("C:\\Users\\Agustin\\Desktop\\TP FINAL LAST\\TP FINAL\\listaTransferencias.ser");
 			 FileInputStream fileIn = new FileInputStream("C:\\Users\\lcoluccio\\Desktop\\GIT\\FinalProject3\\listaTransferencias.ser");
